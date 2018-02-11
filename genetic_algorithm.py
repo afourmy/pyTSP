75c625e20b9ef03c2cd935aae2458d41951249ca
minto <minto@hotmail.fr>
2018-02-11 06:25:41 +0100

    update ga

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -5,6 +5,10 @@ class GeneticAlgorithm(BaseAlgorithm):
     
     def __init__(self):
         super().__init__()
+
+    def crossover_cut(self):
+        first_cut = randint(1, self.size - 2)
+        return first_cut, randint(first_cut + 1, self.size)
     
     ## Mutation methods
 
@@ -12,11 +16,67 @@ class GeneticAlgorithm(BaseAlgorithm):
         i, j = randrange(self.size), randrange(self.size)
         solution[i], solution[j] = solution[j], solution[i]
         return solution
+        
+    def insertion_mutation(self, solution):
+        random_city = randint(0, self.size - 1)
+        random_position = randint(0, self.size)
+        city = solution.pop(random_city)
+        solution.insert(random_position, city)
+    
+    def displacement_mutation(self, solution):
+        a, b = self.crossover_cut()
+        random_position = randint(0, self.size)
+        substring = solution[a:b]
+        solution = solution[:a] + solution[b:]
+        return solution[:random_position] + substring + solution[random_position:]
 
     ## Crossover methods
 
-    def crossover(self, i1, i2):
-        return i1, i2
+    def order_crossover(self, i1, i2):
+        a, b = self.crossover_cut()
+        ni1, ni2, i1, i2 = i1[a:b], i2[a:b], i1[b:] + i1[:b], i2[b:] + i2[:b]
+        for x in i1:
+            if x in ni2: continue
+            ni2.append(x)
+        for x in i2:
+            if x in ni1: continue
+            ni1.append(x)
+        return ni1, ni2
+            
+    def maximal_preservative_crossover(self, i1, i2):
+        c = len(i1)//2
+        r = randint(0, self.size)
+        s1, s2 = (i1*2)[r:r+c], (i2*2)[r:r+c]
+        for x in s1:
+            i2.remove(x)
+        for x in s2:
+            i1.remove(x)
+        ni1, ni2 = s2 + i1, s1 + i2
+        return ni1, ni2
+    
+    def partial_mapping(self, i1, i2, ni1, ni2, a, b):
+        for x in i2[a:b]:
+            if x in i1[a:b]:
+                continue
+            curr = x
+            while True:
+                index_curr = i2.index(curr)
+                j = i1[index_curr]
+                if not ni1[i2.index(j)]:
+                    ni1[i2.index(j)] = x
+                    break
+                else:
+                    curr = ni2[i2.index(j)]
+        
+    def partially_mapped_crossover(self, i1, i2):
+        a, b = self.crossover_cut()
+        ni1, ni2 = [0]*self.size, [0]*self.size
+        ni1[a:b], ni2[a:b] = i1[a:b], i2[a:b]
+        partial_mapping(i1, i2, ni1, ni2, a, b)
+        partial_mapping(i2, i1, ni2, ni1, a, b)
+        ni1 = [x if x else i2[i] for i, x in enumerate(ni1)]
+        ni2 = [x if x else i1[i] for i, x in enumerate(ni2)]
+        return ni1, ni2
 
     ## Core algorithm
     
