d6711278ce8e63b2cd213061a8f31e00ea4aa2ff
minto <minto@hotmail.fr>
2018-02-17 22:31:14 +0100

    pep8

--- algorithms/linear_programming.py
+++ algorithms/linear_programming.py
@@ -3,8 +3,9 @@ from cvxopt import matrix, glpk, solvers
 from itertools import chain, combinations
 from numpy import concatenate, eye, float, full, ones, vstack, zeros
 
+
 class LinearProgramming(BaseAlgorithm):
-    
+
     def edges_to_tour(self, edges):
         tour, current = [], None
         while edges:
@@ -20,15 +21,16 @@ class LinearProgramming(BaseAlgorithm):
                 tour.extend([x, y])
                 current = y
         return tour[:-1]
-    
+
     def ILP_solver(self):
         n, sx = len(distances), len(distances)*(len(distances) - 1)//2
         c = [float(distances[i+1][j+1]) for i in range(n) for j in range(i + 1, n)]
-        G, h, b = [], [], full(n, 2, dtype=float)
-        for st in list(chain.from_iterable(combinations(range(n), r) for r in range(2, n))):
+        G, h, A, b = [], [], [], full(n, 2, dtype=float)
+        for st in chain.from_iterable(combinations(range(n), r) for r in range(2, n)):
             G += [[float(i in st and j in st) for i in range(n) for j in range(i + 1, n)]]
-            h.append(float(len(st)))
-        A = [[float(k in (i, j)) for i in range(n) for j in range(i + 1, n)] for k in range(n)]
+            h.append(float(len(st) - 1))
+        for k in range(n):
+            A.append([float(k in (i, j)) for i in range(n) for j in range(i + 1, n)])
         A, G, b, c, h = map(matrix, (A, G, b, c, h))
         _, x = glpk.ilp(c, G.T, h, A.T, b, B=set(range(sx)))
         reverse_mapping = [(i+1, j+1) for i in range(n) for j in range(i + 1, n)]
