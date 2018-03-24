54adc8ab5f467a68e181ec0199192feeb2934325
minto <minto@hotmail.fr>
2018-03-24 21:01:18 +0100

    update test

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,13 @@
-from models import City
-
-
 def test_pytsp(client):
-    pass
+    assert client.get('/').status_code == 200
+    for algorithm in (
+        'nearest_neighbor',
+        'nearest_insertion',
+        'farthest_insertion',
+        'cheapest_insertion',
+        'pairwise_exchange',
+        'node_insertion',
+        'edge_insertion',
+        'ILP_solver'
+    ):
+        client.post('/' + algorithm)
