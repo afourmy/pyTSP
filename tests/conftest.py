1ea1d7901e7843bb62e97ab6666d5bdfb073b7f7
minto <minto@hotmail.fr>
2018-03-24 20:43:01 +0100

    fix travis and add test

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+from os import remove
+from os.path import abspath, dirname, join, pardir
+from pytest import fixture
+import sys
+
+path_test = dirname(abspath(__file__))
+path_parent = abspath(join(path_test, pardir))
+path_app = join(path_parent, 'source')
+if path_app not in sys.path:
+    sys.path.append(path_app)
+
+from flask_app import create_app
+
+
+@fixture
+def client():
+    app, solver = create_app()
+    client = app.test_client()
+    with app.app_context():
+        yield client
+    remove(join(path_test, 'database.db'))
---
+++ tests/test_base.py
@@ -0,0 +1,5 @@
+from models import City
+
+
+def test_pytsp(client):
+    pass
